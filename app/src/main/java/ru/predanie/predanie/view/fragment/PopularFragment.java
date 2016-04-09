164c4b9fde399c83e95e3e5740d0112ea23987f1
nipol <artram@yandex.ru>
2016-04-09 19:20:24 +0500

    Snackbar retry

--- app/src/main/java/ru/predanie/predanie/view/fragment/PopularFragment.java
+++ app/src/main/java/ru/predanie/predanie/view/fragment/PopularFragment.java
@@ -1,6 +1,7 @@
 package ru.predanie.predanie.view.fragment;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 import com.annimon.stream.Stream;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -45,6 +48,8 @@ public class PopularFragment extends Fragment {
   private RecyclerView creationsRecycleView;
   private RecyclerView.LayoutManager layoutManager;
   private CreationsRecycleAdapter creationsRecycleAdapter;
+  private ProgressBar progressBar;
+  private FrameLayout frameLayout;
 
   private List<Composition> compositionList;
   private IApi compositionApi;
@@ -63,18 +68,6 @@ public class PopularFragment extends Fragment {
 
   @Override public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
-    Call<List<Composition>> call = compositionApi.getCompositions("popular", queryMap);
-    call.enqueue(new Callback<List<Composition>>() {
-      @Override
-      public void onResponse(Call<List<Composition>> call, Response<List<Composition>> response) {
-        Stream.of(response.body()).forEach(compositionList::add);
-      }
-
-      @Override public void onFailure(Call<List<Composition>> call, Throwable t) {
-        Log.d(TAG, "GetPopular Network fetch failure");
-      }
-    });
   }
 
   @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -84,12 +77,36 @@ public class PopularFragment extends Fragment {
     setRetainInstance(true);
 
     creationsRecycleView = (RecyclerView) rootView.findViewById(R.id.creationsRecyclerView);
-    creationsRecycleAdapter = new CreationsRecycleAdapter(compositionList);
-    creationsRecycleView.setAdapter(creationsRecycleAdapter);
+    progressBar = (ProgressBar) rootView.findViewById(R.id.popular_spinner);
+    frameLayout = (FrameLayout) rootView.findViewById(R.id.popularRootLayout);
+
     layoutManager = new LinearLayoutManager(getActivity());
     creationsRecycleView.setLayoutManager(layoutManager);
+    getCompositions();
 
     return rootView;
   }
 
+  private void getCompositions() {
+    progressBar.setVisibility(View.VISIBLE);
+    Call<List<Composition>> call = compositionApi.getCompositions("popular", queryMap);
+    call.enqueue(new Callback<List<Composition>>() {
+      @Override
+      public void onResponse(Call<List<Composition>> call, Response<List<Composition>> response) {
+        Stream.of(response.body()).forEach(compositionList::add);
+        creationsRecycleAdapter = new CreationsRecycleAdapter(compositionList);
+        creationsRecycleView.setAdapter(creationsRecycleAdapter);
+        progressBar.setVisibility(View.GONE);
+      }
+
+      @Override public void onFailure(Call<List<Composition>> call, Throwable t) {
+        progressBar.setVisibility(View.GONE);
+        Log.d(TAG, "GetPopular Network fetch failure");
+        Snackbar.make(frameLayout, getString(R.string.error_network), Snackbar.LENGTH_INDEFINITE)
+            .setAction(getString(R.string.retry), v -> {
+                getCompositions();
+            }).show();
+      }
+    });
+  }
 }
